13d59173a1f0314e8d81b8df7e5aff044fd3dce0
sonurajp <sonurajp49@gmail.com>
2023-09-08 23:54:14 +0530

    create userController for registration API

---
+++ backend/controllers/userControllers.js
@@ -0,0 +1,33 @@
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password, pic } = req.body;
+  if (name || email || password) {
+    res.status(400);
+    throw new Error("Please enter all the Fields");
+  }
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists!!!");
+  }
+  const user = User.create({
+    name,
+    email,
+    password,
+    pic,
+  });
+  if (user) {
+    res.status(201).json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+      password: user.password,
+      pic: user.pic,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Failed To Create User!!!");
+  }
+});
+module.exports = { registerUser };
