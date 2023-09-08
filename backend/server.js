49f143ac92e959d39d787ebc5de3c996e14b26f0
sonurajp <sonurajp49@gmail.com>
2023-09-08 23:18:56 +0530

    Setup userRoutes file

--- backend/server.js
+++ backend/server.js
@@ -4,18 +4,13 @@ const app = express();
 const { chats } = require("./data/data");
 const connectDB = require("./config/db");
 const colors = require("colors");
-
+const userRoutes = require("./routes/userRoutes");
 dontnev.config();
 connectDB();
 app.get("/", (req, res) => {
   res.send("App is running");
 });
-app.get("/api/chat", (req, res) => {
-  res.send(chats);
-});
-app.get("/api/chat/:id", (req, res) => {
-  const singleChat = chats.find((c) => c._id === req.params.id);
-  res.send(singleChat);
-});
+
+app.use("/api/user", userRoutes);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`server has started in ${PORT}`.yellow.bold));
