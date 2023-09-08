a2e8a7fdfb6dbc29846f77154b44415d7f54b67e
sonurajp <sonurajp49@gmail.com>
2023-09-09 01:30:47 +0530

    back end add error middleware

---
+++ backend/middleware/errorMiddleware.js
@@ -0,0 +1,16 @@
+//@description     Get or Search all users
+//@route           GET /api/user?search=
+//@access          Public
+const allUsers = asyncHandler(async (req, res) => {
+  const keyword = req.query.search
+    ? {
+        $or: [
+          { name: { $regex: req.query.search, $options: "i" } },
+          { email: { $regex: req.query.search, $options: "i" } },
+        ],
+      }
+    : {};
+
+  const users = await User.find(keyword).find({ _id: { $ne: req.user._id } });
+  res.send(users);
+});
