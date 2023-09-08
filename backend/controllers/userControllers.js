fb2f9c819bf6ff6d604e2bb38e18c4bb34a58e19
sonurajp <sonurajp49@gmail.com>
2023-09-09 01:25:21 +0530

    Backend login api se up

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -42,4 +42,26 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 });
 
-module.exports = { registerUser };
+//@description     Auth the user
+//@route           POST /api/users/login
+//@access          Public
+const authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      pic: user.pic,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid Email or Password");
+  }
+});
+
+module.exports = { allUsers, registerUser, authUser };
