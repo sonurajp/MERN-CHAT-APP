7d69bf9410133f495bd2b4bb32e01ed24d484b8d
sonurajp <sonurajp49@gmail.com>
2023-09-11 23:45:07 +0530

    backend- add search user logic in userController and authMiddleware

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -2,6 +2,23 @@ const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
 const generateToken = require("../config/generateToken");
 
+//@description     Get or Search all users
+//@route           GET /api/user?search=
+//@access          Public
+const allUsers = asyncHandler(async (req, res) => {
+  const keyword = req.query.search
+    ? {
+        $or: [
+          { name: { $regex: req.query.search, $options: "i" } },
+          { email: { $regex: req.query.search, $options: "i" } },
+        ],
+      }
+    : {};
+
+  const users = await User.find(keyword).find({ _id: { $ne: req.user._id } });
+  res.send(users);
+});
+
 //@description     Register new user
 //@route           POST /api/user/
 //@access          Public
@@ -49,6 +66,7 @@ const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
 
   const user = await User.findOne({ email });
+
   if (user && (await user.matchPassword(password))) {
     res.json({
       _id: user._id,
@@ -64,4 +82,4 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
-module.exports = { registerUser, authUser };
+module.exports = { allUsers, registerUser, authUser };
